3748aa58da02e102e752bde487be5f678ac5991c
Rita Mernissi <mernissi.rita@gmail.com>
2024-06-26 23:38:22 +0200

    hide documents on mobile

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Chat } from "@/components/layout/Chat";
 import { Search } from "@/components/layout/Search";
 import { RetrievedFiles } from "@/components/layout/RetrievedFiles";
@@ -11,17 +11,29 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/componen
 
 const HomePage = () => {
     const [selectedSymbol, setSelectedSymbol] = useState<string>("");
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+
+    useEffect(() => {
+        const checkIfMobile = () => {
+            setIsMobile(window.innerWidth < 768); // Adjust this breakpoint as needed
+        };
+
+        checkIfMobile();
+        window.addEventListener('resize', checkIfMobile);
+
+        return () => window.removeEventListener('resize', checkIfMobile);
+    }, []);
 
     return (
         <OpenaiProvider>
-            <main className="flex flex-col h-screen p-12 px-4 bg-gray-50">
+            <main className="flex flex-col h-screen p-4 md:p-12 bg-gray-50">
                 <div className="flex-none mb-4">
                     <Search onSymbolSelect={setSelectedSymbol} />
                 </div>
 
                 <MessagesProvider>
                     <div className="flex-grow overflow-hidden">
-                        {selectedSymbol ? (
+                        {!isMobile && selectedSymbol ? (
                             <ResizablePanelGroup direction="horizontal" className="h-full rounded-lg border">
                                 <ResizablePanel defaultSize={50} minSize={30}>
                                     <ScrollArea className="h-full pr-4">
